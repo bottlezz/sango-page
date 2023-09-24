e9b2be2a8407d22f4a284921f4dde7b56b118aae
Greg Zhou <bottlezz2002@gmail.com>
2023-09-24 04:16:27 -0700

    update

--- src/wc/sgJiang.js
+++ src/wc/sgJiang.js
@@ -120,6 +120,8 @@ class SgJiang extends HTMLElement {
     const nameSpan = this.shadowRoot.querySelector(".jiang-name");
     const hpSpan = this.shadowRoot.querySelector(".jiang-hp");
     const frontDiv = this.shadowRoot.querySelector(".card-front");
+    frontDiv.style.backgroundImage = 'url("imgs/j.jpg")';
+    // frontDiv.style.backgroundColor = "yellow";
 
     nameSpan.innerHTML = jiang.name;
     hpSpan.innerHTML = jiang.health;
